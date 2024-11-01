98d500d855dbe4e6074b948d31e32c6c05eadee9
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2024-11-01 17:19:28 +0200

    Add files via upload

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AppLogger;
+internal class Program
+{
+    private Logger logger = new Logger();
+    private static void Main(string[] args)
+    {
+        Logger.Log("Tulosta tekstiä!");
+        string text = "Tulostetaan lisää tekstiä";
+        Logger.Log(text);
+    }
+}
